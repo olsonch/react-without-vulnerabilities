44d3d126bbf514bf6b0fef4186faf8e1eeee748e
Chris Olson <chris.olson@dsu.edu>
2021-12-04 11:42:01 -0600

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+    return (
+        <div className={'container text-center'}>
+            <h1>Hello World</h1>
+        </div>
+    )
+}
+
+export default App;
